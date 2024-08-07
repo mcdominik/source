360cced38e4683461992f1dfaf47191bf0d51b36
mcdominik <dominik.mackiewicz@icloud.com>
2024-08-07 11:59:05 +0200

    clean branch

--- src/api-proxy/api-proxy.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApiProxyService } from './api-proxy.service';
-import { ApiProxyController } from './api-proxy.controller';
-
-@Module({
-  controllers: [ApiProxyController],
-  providers: [ApiProxyService],
-})
-export class ApiProxyModule {}
